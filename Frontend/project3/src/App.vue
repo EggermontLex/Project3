1a00a3981a338a638158ec03be1c27a6de598317
BogaertsCeline <celine.bogaerts@student.howest.be>
2019-05-30 15:26:31 +0200

    button component

--- Frontend/project3/src/App.vue
+++ Frontend/project3/src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Button text="dit is tekst!!!!"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Button
   }
 }
+
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'CircularStd', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-Black.ttf);
+  font-weight: 900;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-BlackItalic.ttf);
+  font-weight: 900;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-Bold.ttf);
+  font-weight: 700;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-BoldItalic.ttf);
+  font-weight: 700;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-Medium.ttf);
+  font-weight: 500;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: CircularStd;
+  src: url(assets/fonts/CircularStd-MediumItalic.ttf);
+  font-weight: 500;
+  font-style: italic;
 }
 </style>
---
+++ Frontend/project3/src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button type="button" name="button" class="button">{{text}}</button>
+</template>
+<script>
+export default {
+    name: 'Button',
+    props: {
+    text: String
+  }
+    
+}
+</script>
+<style scoped>
+.button{
+  background-color: #FFF;
+  padding: 14px 45px;
+  border-radius: 5px;
+  box-shadow: 0 2px 0 rgba(0,0,0,.2) ;
+  color: #006ab3;
+  border: none;
+  font-family: CircularStd;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+</style>
+
