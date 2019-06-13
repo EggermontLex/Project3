86886ef5a8791f06b05c6b214f562c80b1401434
Delaet Ruben <ruben.delaet@student.howest.be>
2019-06-13 15:58:57 +0200

    Aanapssen van de run mogelijkheden van centroidtracker

--- MachineLearning/tracking-centroid.py
+++ MachineLearning/tracking-centroid.py
@@ -5,33 +5,31 @@ from PIL import ImageDraw
 import numpy as np
 import cv2
 import warnings
-from google.cloud import pubsub_v1
 import os
 import datetime
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/mendel/project3/Project3-ML6-515024366790.json"
-data = ""
-f= open("cache.txt","a")
 
-from tools.cloud_manager import CloudManager
 
 
 from pyimagesearch.centroidtracker import CentroidTracker
 from collections import deque
-from deep_sort import preprocessing
-from deep_sort import nn_matching
-from deep_sort.detection import Detection
-from deep_sort.tracker import Tracker
-#from tools import generate_detections as gdet
-from deep_sort.detection import Detection as ddet
 warnings.filterwarnings('ignore')
 
+from tools.cloud_manager import CloudManager
 
+
+#aanmaken cloud manager
 project_id = "Project3-ML6"
 topic_name = "data_register"
 publisher = CloudManager(project_id,topic_name)
 
 
-# Function to read labels from text files.
+flag_invert=True
+flag_video = False
+flag_fps = True
+
+
+#Function to read labels from text files.
 def ReadLabelFile(file_path):
     with open(file_path, 'r', encoding="utf-8") as f:
         lines = f.readlines()
@@ -43,26 +41,24 @@ def ReadLabelFile(file_path):
 
 
 def main():
-    max_cosine_distance = 1
-    nn_budget = None
-    nms_max_overlap = 1.0
+
+    #declaratie gebruikte variabelen
+    t1 =0.0
     fps = 0.0
     persons_in = 0
     line_trail = dict()
 
-    # Initialize engine.
+    #Initialize engine.
     engine = DetectionEngine('model_tflite/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
     labels = ReadLabelFile('model_tflite/coco_labels.txt')
 
-    # Deep sort
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
-    #centroid tracker
+    #instantie aanmaken van de centroid tracker
     ct = CentroidTracker()
+
+    #capture videocamera
     cap = cv2.VideoCapture(1)
-    
-    writeVideo_flag = True
-    if writeVideo_flag:
+
+    if flag_video:
         w = int(cap.get(3))
         h = int(cap.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
@@ -71,13 +67,11 @@ def main():
         frame_index = -1
 
 
-    invert = True
-
     while True:
         ret, frame = cap.read()
-        t1 = time.time()
-        if ret:
+        if flag_fps: t1 = time.time()
 
+        if ret:
             cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(cv2_im)
             width, height = img.size
@@ -90,27 +84,25 @@ def main():
             # Display result.
             if ans:
                 for obj in ans:
-                    #if labels: print(labels[obj.label_id])
                     box = obj.bounding_box.flatten().tolist()
-                    # Draw a rectangle.
-                    draw.rectangle(box, outline='red')
-                    #draw.text((box[0], box[1]), labels[obj.label_id])
-                    #draw.text((box[0], box[1] + 10), str(obj.score))
+                    if flag_video: draw.rectangle(box, outline='red')
                     boxs.append(box)
 
             objects = ct.update(boxs)
 
-            draw.line((0, line1, width, line1), fill=10, width=5)
-            img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            if flag_video:
+                draw.line((0, line1, width, line1), fill=10, width=5)
+                img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
             for (objectID, centroid) in objects.items():
                 #line_order[objectID] = deque(maxlen=2)
                 if objectID not in line_trail.keys():
                     line_trail[objectID] = deque(maxlen=2)
-                text = "ID {}".format(objectID)
-                cv2.putText(img, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255),4)
-                cv2.circle(img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-                cv2.line(img, (0, centroid[2]), (width, centroid[2]), (255, 0, 0), 2)
+                if flag_video:
+                    text = "ID {}".format(objectID)
+                    cv2.putText(img, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255),4)
+                    cv2.circle(img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+                    cv2.line(img, (0, centroid[2]), (width, centroid[2]), (255, 0, 0), 2)
                 center = (centroid[1], centroid[2])
                 line_trail[objectID].appendleft(center)
                 try:
@@ -118,14 +110,14 @@ def main():
                     if diff < 60:
                         if line_trail[objectID][0][1] < int(line1) and line_trail[objectID][1][1] > int(line1):
                             #binnen() if invert else buiten()
-                            if invert:
+                            if flag_invert:
                                 persons_in += 1
                                 publisher.publish_to_topic_new(data = ("+1,%s" % datetime.datetime.now()))
                             else:
                                 persons_in -= 1
                                 publisher.publish_to_topic_new(data = ("-1,%s" % datetime.datetime.now()))
                         elif line_trail[objectID][1][1] < int(line1) and line_trail[objectID][0][1] > int(line1):
-                            if invert:
+                            if flag_invert:
                                 publisher.publish_to_topic_new(data = ("-1,%s" % datetime.datetime.now()))
                                 persons_in -= 1
                             else:
@@ -134,19 +126,17 @@ def main():
                 except Exception as Ex:
                     pass
 
-            fps = (fps + (1. / (time.time() - t1))) / 2
-            cv2.putText(img, "Binnen: " + str(persons_in), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),lineType=cv2.LINE_AA)
-            cv2.putText(img, "fps: " + str(int(fps)), (260, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),lineType=cv2.LINE_AA)
+            if flag_fps: print("fps : %d" % ((fps + (1. / (time.time() - t1))) / 2))
+            if flag_video:
+                cv2.putText(img, "Binnen: " + str(persons_in), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),lineType=cv2.LINE_AA)
+                cv2.putText(img, "fps: " + str(int(fps)), (260, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),lineType=cv2.LINE_AA)
 
-            if writeVideo_flag:
-                # save a frame
+            if flag_video:
                 out.write(img)
+                cv2.imshow('preview', img)
 
-
-            cv2.imshow('preview', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            print(persons_in)
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
